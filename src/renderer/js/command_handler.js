50b560c3ef561960521690fed51e2a3fb263cbc5
UmutTurk24 <umturk@davidson.edu>
2023-03-19 10:35:33 -0400

    Backspace

--- src/renderer/js/command_handler.js
+++ src/renderer/js/command_handler.js
@@ -4,6 +4,7 @@ const command_output_element = document.getElementById("command_text");
 window.commandTransfer.receive('fromMain', function (data){
     data.op_result['rawHTML'].rawHTML = data.op_result['rawHTML'].rawHTML.replace(/\t/g, '&emsp');
     data.op_result['rawHTML'].rawHTML = data.op_result['rawHTML'].rawHTML.replace(/\n/g, '<br>');
+    data.op_result['rawHTML'].rawHTML = data.op_result['rawHTML'].rawHTML.replace(' ', '&nbsp');
     command_title_element.innerHTML = "Executed Command: " + data.operation;
     command_output_element.innerHTML = data.op_result['rawHTML'].rawHTML;
 
